a55931560e2e3b2ea0aaaff17d0a0b9d7f13083d
Jiahong Rong <jiahongrong@jiahongs-mbp.attlocal.net>
2018-04-22 16:30:04 -0500

    code a little

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+var redCrystal = Math.floor(Math.random() * (12 - 1)) + 1;
+var blueCrystal = Math.floor(Math.random() * (12 - 1)) + 1;
+var yellowCrystal = Math.floor(Math.random() * (12 - 1)) + 1;
+var greenCrystal = Math.floor(Math.random() * (12 - 1)) + 1;
+
+
+
+$("img#red").on("click" , function(){
+    alert("you clicked");
+});
