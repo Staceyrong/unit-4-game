15908515dcbb4fa5ab27b986149ba649ffc15b4a
Jiahong Rong <jiahongrong@jiahongs-mbp.attlocal.net>
2018-04-22 19:49:48 -0500

    finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,58 +7,53 @@ var sum = 0;
 var win = 0;
 var loose = 0;
 
+function reset(){
+    computerNumber = Math.floor(Math.random() * (120 - 19)) + 19;
+    $("#number").text(computerNumber);
+    redCrystal = Math.floor(Math.random() * (12 - 1)) + 1;
+    blueCrystal = Math.floor(Math.random() * (12 - 1)) + 1;
+    yellowCrystal = Math.floor(Math.random() * (12 - 1)) + 1;
+    greenCrystal = Math.floor(Math.random() * (12 - 1)) + 1;
+    sum = 0;
+}
+
 $(document).ready(function(){
     $("#number").text(computerNumber);
 
     $("img").on("click" , function(){
-        var input = $(this).attr("value");
+        var input = $(this).attr("id");
         console.log(input);
-        if (input == 'redCrystal'){
+        if (input == "red"){
             sum += redCrystal;
             $("#yourNumber").text(sum);
         }
-        else {
-            sum += greenCrystal;
+        else if (input == "blue"){
+            sum += blueCrystal;
             $("#yourNumber").text(sum);
         }
-
-    // });
-
-    // $("img#blue").on("click" , function(){
-    //     sum += blueCrystal;
-    //     $("#yourNumber").text(sum);
-
-    // });
-
-    // $("img#yellow").on("click" , function(){
-    //     sum += yellowCrystal;
-    //     $("#yourNumber").text(sum);
-
-    // });
-
-    // $("img#green").on("click" , function(){
-    //     sum += greenCrystal;
-    //     $("#yourNumber").text(sum);
-
-    // });
-
+        else if (input == "yellow"){
+            sum += yellowCrystal;
+            $("#yourNumber").text(sum);
+        }
+        else if (input == "green"){
+            sum += greenCrystal;
+            $("#yourNumber").text(sum);
+        } 
 
     if (sum == computerNumber) {
         win++;
         $("#winTimes").text(win);
-        redCrystal = Math.floor(Math.random() * (12 - 1)) + 1;
-        blueCrystal = Math.floor(Math.random() * (12 - 1)) + 1;
-        yellowCrystal = Math.floor(Math.random() * (12 - 1)) + 1;
-        greenCrystal = Math.floor(Math.random() * (12 - 1)) + 1;
-        computerNumber = Math.floor(Math.random() * (120 - 19)) + 19;
+        reset();
         console.log('win!!!!')
     }
     if (sum > computerNumber) {
+        loose++;
+        $("#looseTimes").text(loose);
+        reset();
         console.log(computerNumber)
-        console.log(sum)
-        
+        console.log(sum)    
     }
-});
+    });
 
 
 });
